f042950b8743418b283b0c2c119d4d82ca11085b
kulik-lab <w.kulik@posteo.net>
2021-09-12 17:08:23 +0200

    initial commit

---
+++ pointers-1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    int x = 5;
+    std::cout << "value" << x << "\n";
+    std::cout << "address" << &x << "n";
+    char ch = getchar(); // wait for key
+}
+
+/* output:
+
+*/
